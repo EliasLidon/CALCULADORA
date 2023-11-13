eb90b91cd8ae22cc11cea1adfc78788fdce7186f
Elias Silva <lidioneelias@gmail.com>
2023-11-13 20:46:38 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CALCULATOR',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
